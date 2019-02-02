f3e37ae880af46b44c5db9f5d0ddc28318b64552
Balazs Tar <balazs.tar@gmail.com>
2019-02-02 15:52:37 +0100

    v46

--- Player.js
+++ Player.js
@@ -3,7 +3,7 @@ const {getChances, isWorthIt} = require('./Odds')
 
 class Player {
   static get VERSION() {
-    return '0.45';
+    return '0.46';
   }
 
   static betRequest(gameState, bet) {
@@ -20,10 +20,11 @@ class Player {
       // console.log('@@@cards numrank', getRankNum(cards[0].rank, cards[1].rank), getRankNum(cards[1].rank, cards[0].rank))
       const handChances = getChances(getCards(gameState), gameState.community_cards)
       if (isPreFlop) {
+        
         if (isGood/*&& !isHighRiskValue*/) {
           betValue = value * 1.20
         } else if (!isHighRiskValue) {
-          betValue = value
+          betValue = value + (gameState.small_blind * 2)
         }
       } else {
         try {
