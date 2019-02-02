80f988078596026ee18986e5e42c9b0073204196
Balazs Tar <balazs.tar@gmail.com>
2019-02-02 12:00:59 +0100

    gamestate logging

--- Player.js
+++ Player.js
@@ -1,6 +1,6 @@
 class Player {
   static get VERSION() {
-    return '0.22';
+    return '0.23';
   }
 
   static getRankNum (rank) {
@@ -27,16 +27,17 @@ class Player {
       const me = gameState.players[gameState.in_action]
       const riskValue = parseInt(me.stack * 0.1, 10)
       const value = gameState.minimum_raise || gameState.current_buy_in
+      const betValue = value <= riskValue ? value : 0
+      console.log('@@@ gameState', gameState);
       console.log(
         '@@@', gameState.game_id,
         'stack',  me.stack,
         'riskValue', riskValue,
         'minRaise', gameState.minimum_raise,
         'buyIn', gameState.current_buy_in,
-        'value', value
+        'value', value,
+        'betValue', betValue
       );
-      const betValue = value <= riskValue ? value : 0
-      console.log('@@@ betValue', betValue);
       bet(betValue);
     } catch (e) {
       console.log('@@@error', e)
